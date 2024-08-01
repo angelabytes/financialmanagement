9ed24b333df62622051a415b7c80e90c95420f23
Angela <89052245+serenitysprayer@users.noreply.github.com>
2024-08-01 11:19:07 -1000

    added methods to financial operations

--- src/main/java/FinancialOperations.java
+++ src/main/java/FinancialOperations.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FinancialOperations {
     //public static double getHourlyRateSum(List<Employee> employees) {
@@ -9,11 +11,36 @@ public class FinancialOperations {
     //    return sum;
     //}
     //
-    public static double getMonthlyRateSum(List<Employee> employees) {
+    public static double getHourlyRateSum(List<Employee> employees) {
         return employees.stream().mapToDouble(employee -> employee.getSalary()).sum();
     }
-    public static double sumDepartmentSalary(List<Employee> employees, String department){
-        return employees.stream().filter(e -> {e.getDepartment()).equals(department)}
 
+    public static double sumDepartmentSalary(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .mapToDouble(Employee::getSalary)
+                .sum();
     }
+
+    public static double getAverageSalaryByDepartment(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .mapToDouble(Employee::getSalary)
+                .average()
+                .orElse(0.0);
+    }
+    public static double sumDepartmentHourlyRate(List<Employee> employees, String department) {
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .mapToDouble(Employee::getHourly_rate)
+                .sum();
+    }
+    public static double getAverageHourlyRateByDepartment(List<Employee> employees, String department) {
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .mapToDouble(Employee::getHourly_rate)
+                .average()
+                .orElse(0.0);
+    }
+
 }
